09749d118b41fc04e2cebb9d73cf41f2b63282d2
Nicholas <nichsmm@gmail.com>
2017-06-06 11:07:46 -0300

    Add keyboard scrolling to base View Controller

--- Boilerplate/Modules/Base/View/BaseViewController.swift
+++ Boilerplate/Modules/Base/View/BaseViewController.swift
@@ -15,6 +15,8 @@ class BaseViewController: UIViewController, BaseView {
     
     var progressHUD: MBProgressHUD?
     
+    fileprivate var internalScrollView: UIScrollView?
+    
     // MARK: Methods
     
     func showLoading() {
@@ -42,4 +44,34 @@ class BaseViewController: UIViewController, BaseView {
         present(errorController, animated: true, completion: nil)
     }
     
+    func setupKeyboardNotifications(with scrollView: UIScrollView?) {
+        internalScrollView = scrollView
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    // MARK: Keyboard Notifications
+    func keyboardWillShow(notification: NSNotification) {
+        guard let info = notification.userInfo,
+              let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size else { return }
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height + 15.0, right: 0.0)
+        
+        internalScrollView?.contentInset = contentInsets
+        internalScrollView?.scrollIndicatorInsets = contentInsets
+        
+        var aRect = view.frame
+        aRect.size.height -= keyboardSize.height
+        guard let activeFieldPresent = findActiveTextField(view.subviews) else { return }
+        
+        if (!aRect.contains(activeFieldPresent.frame.origin))
+        {
+            internalScrollView?.scrollRectToVisible(activeFieldPresent.frame, animated: true)
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        internalScrollView?.contentInset = contentInsets
+        internalScrollView?.scrollIndicatorInsets = contentInsets
+    }
 }
